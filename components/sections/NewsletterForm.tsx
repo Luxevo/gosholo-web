d0f25106a3b35e86abeacc212a5ad977112f8ad5
JolanJ <jolan@luxevotech.com>
2025-06-19 16:14:05 -0400

    brevo

--- components/sections/NewsletterForm.tsx
+++ components/sections/NewsletterForm.tsx
@@ -24,8 +24,7 @@ export function NewsletterForm() {
     <iframe 
       width="100%"
       height="100%"
-      src="https://ee01743d.sibforms.com/serve/MUIFAGTGwsEmvfYlwMYRYHzH8N_FNRm0Dp0Rg5BsmLQmxJ1DONObyhqkb8JeoO3ytGaki8tYiSov7jRv0aZcBmq4TumEdjRLvG2_gEonSnOC-f51WNxBrV9KdSNBpcbm8x79M9xn5B3QsrC3K4x76YIrZMV_erpLsVBw61vhcmUgktMxwyGS7xUGleYGAdjuMyT5ZIBk_emByfHu"      frameBorder="0"
-      scrolling="auto"
+      src="https://ee01743d.sibforms.com/serve/MUIFAJh33dDDKPxu6CEXIbceRcy_PF43wffiXk7MEoSvDZ38RMgoi4DqYOaKmA5cU-4d9MXkFA1LQO9N7HnMCio8FqkNhH_4m0hoNuwcPxpEYrie4qq87FE0TaqLPTRWWEzK8RfgHRyNl80K4VJdF4BwyYTuqFvjBOXRo-wiSJ0UaYuETlJBc4rUcjxOa2SOEKURXMTRaKye9Aro"      scrolling="auto"
       allowFullScreen
       style={{
         display: "block",
